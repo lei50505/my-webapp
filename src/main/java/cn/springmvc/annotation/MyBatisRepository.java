47ff57daba05d5b54579c5bf0c3632d2b7bb5ee3
lei.cao <lei.cao02@ele.me>
2015-12-25 14:19:10 +0800

    使用配置dao

--- src/main/java/cn/springmvc/annotation/MyBatisRepository.java
+++
@@ -1,5 +0,0 @@
-package cn.springmvc.annotation;
-
-public @interface MyBatisRepository {
-
-}
--- src/main/java/cn/springmvc/dao/UserDao.java
+++ src/main/java/cn/springmvc/dao/UserDao.java
@@ -1,9 +1,7 @@
 package cn.springmvc.dao;
 
-import cn.springmvc.annotation.MyBatisRepository;
 import cn.springmvc.entity.User;
 
-@MyBatisRepository
 public interface UserDao {
     public void insert(User user);
 }
